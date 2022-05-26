48fa39659db33cbf147b14db75252aa5ab1d5d31
Emmanuel Garzon <eggarzon@gmail.com>
2022-05-25 23:23:38 -0700

    first module completed, initial commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  // const expenseDate = new Date(2021, 2, 28); // returns as an object
+  // const expenseTitle = "Entertainment System";
+  // const expenseAmount = 1750.0;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
