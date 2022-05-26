e7d79fc7c10cf5d57512eda06e6608c49e212ab4
Emmanuel Garzon <eggarzon@gmail.com>
2022-05-26 13:41:27 -0700

    Section 4, work in progress

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
@@ -7,13 +9,24 @@ const ExpenseItem = (props) => {
   // const expenseTitle = "Entertainment System";
   // const expenseAmount = 1750.0;
 
+  const [title, setTitle] = useState(props.title); // returns an array with exactly TWO elements, current value, and a function
+
+  // let title = props.title; // value cannot be reassigned traditionally, see above from useState
+
+  const clickHandler = () => {
+    setTitle("Updated Successfully!"); // function assigns new value, and calls the component function again to be reevaluated
+    // title = "Updated!";
+    console.log(title);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
